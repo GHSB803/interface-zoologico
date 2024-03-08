bbcd64ca0c25ea15d028c5eb47eec40c95fc4286
GHSB803 <gabrielhenri9427@gmail.com>
2024-03-08 16:01:53 -0300

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import './App.css'
-import Navegacao from './components/Nacegacao/Navegacao'
-import Atracao from './components/Atracao/Atracao'
+import Animais from './pages/Animais/Animais';
+import Depoimentos from './pages/Depoimentos/Depoimentos';
+import Fotos from './pages/Fotos/Fotos';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import { BrowserRouter as Roteador, Routes, Route } from 'react-router-dom'
+
 
 function App() {
-    }
+
+
   return (
     <>
-      <div>
-        </div>
-    </>
-  )
+      <Roteador>
+        <Routes>
 
+          <Route exact path='/' Component={Home} />
+          <Route exact path='/animais' Component={Animais} />
+          <Route exact path='/Fotos' Component={Fotos} />
+          <Route exact path='/Depoimentos' Component={Depoimentos} />
+          <Route exact path='/Login' Component={Login} />
+
+        </Routes>
+      </Roteador>
+    </>
+  );
+}
 
 export default App
--- src/components/Atracao/Atracao.jsx
+++ src/components/Atracao/Atracao.jsx
@@ -1,11 +1,11 @@
-import imgNotFound from '../../assets/img-not-found.png'
+import imgNotFound from '../../assets/img-not-found.png'; // Importando recurso para o componente
 import './Atracao.css'
 
-function Atracao(){
-    return(
+function Atracao() {
+    return (
         <div className="ctn-atracao">
-            <img src={imgNotFound}/>
-            <p>Lorem Ipsum</p>       
+            <img src={imgNotFound} />
+            <p>Lorem Ipsum</p>
         </div>
     );
 }
